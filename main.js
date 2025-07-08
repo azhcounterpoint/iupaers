92f7b391e8d87c9c58463f8ee91468ab12580e9b
azhcounterpoint <azhcounterpoint@gmail.com>
2025-07-08 14:45:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAV0ZgEYMupAu9b8xZRuNV00HUouElztrw",
-  authDomain: "iupaers.firebaseapp.com",
-  databaseURL: "https://iupaers-default-rtdb.firebaseio.com",
-  projectId: "iupaers",
-  storageBucket: "iupaers.firebasestorage.app",
-  messagingSenderId: "988099496172",
-  appId: "1:988099496172:web:2a8a39ebe6c7d2ac257dc9"
+  apiKey: "AIzaSyCGWl_EXifTTEugTD3RhjHnTB01iqBrxKU",
+  authDomain: "iupaers-8e7d4.firebaseapp.com",
+  databaseURL: "https://iupaers-8e7d4-default-rtdb.firebaseio.com",
+  projectId: "iupaers-8e7d4",
+  storageBucket: "iupaers-8e7d4.firebasestorage.app",
+  messagingSenderId: "816440688463",
+  appId: "1:816440688463:web:6303dd1a75191367928f3a"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
